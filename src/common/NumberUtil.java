87d300f3c48bb53aa11bb636f50c0b71efc57d03
nori <nori@LAPTOP-NP66HTLG>
2017-03-05 01:46:55 +0900

    乱数追加

--- src/common/NumberUtil.java
+++ src/common/NumberUtil.java
@@ -1,8 +1,21 @@
 package common;
 
+import java.util.Random;
+
 /**
  * 数値ユーティリティ
  */
 public class NumberUtil {
+
+	/**
+	 * 乱数を取得する
+	 *
+	 * @param i
+	 * @return 1から引数までの整数
+	 */
+	public static int getRndNo(int i) {
+		Random r = new Random();
+		return r.nextInt(i) + 1;
+	}
 
 }
